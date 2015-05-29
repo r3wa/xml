e5e141d7b6bd1300ec6b1fb13715896ac54c99d5
Robson Duarte <robson.o.d@gmail.com>
2015-05-29 17:37:22 -0300

    add basics att in the class and comments and create readme.

--- jaxb-xsd-generate/src/main/java/br/com/r3wa/integration/message/Header.java
+++ jaxb-xsd-generate/src/main/java/br/com/r3wa/integration/message/Header.java
@@ -3,9 +3,30 @@ package br.com.r3wa.integration.message;
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 
+/**
+ * 
+ * This is the Header of {@link Message}
+ * 
+ * @author r3wa
+ *
+ */
 @XmlAccessorType(FIELD)
 public class Header {
 
+	/**
+	 * this is the number of request example
+	 */
+	@XmlElement(required=true)
+	private long number;
+	
+	
+	/**
+	 * this is the status of request example
+	 */
+	@XmlElement(required=true)
+	private String status;
 
+	
 }
--- jaxb-xsd-generate/src/main/java/br/com/r3wa/integration/message/Line.java
+++ jaxb-xsd-generate/src/main/java/br/com/r3wa/integration/message/Line.java
@@ -3,8 +3,30 @@ package br.com.r3wa.integration.message;
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 
+/**
+ * This is the line o {@link Message}
+ * 
+ * 
+ * @author r3wa
+ *
+ */
 @XmlAccessorType(FIELD)
 public class Line {
 
+	/**
+	 * this is the code of line example
+	 */
+	@XmlElement(required=true)
+	private String code;
+	
+	
+	/**
+	 * this is the value of line example
+	 */
+	@XmlElement(required=true)
+	private double value;
+	
+	
 }
